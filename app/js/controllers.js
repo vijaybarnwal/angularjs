3ce5ceaf4c0f50a7a2b5559ae13ae933cfd9ecd0
Vijay Barnwal <manusis@satyaatmanusis.(none)>
2013-03-20 14:14:38 +0530

    commit all tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,10 +1,13 @@
 'use strict';
 
 /* Controllers */
-function PhoneListCtrl($scope , $http) {
-	$http.get('phones/phones.json').success(function(data){
-		$scope.phones = data.splice(0, 5);
-	});
+function PhoneListCtrl($scope , phone) {
+	$scope.hello = "Hello,Welcome to Mobile World!"
+	// $http.get('phones/phones.json').success(function(data){
+		// $scope.phones = data.splice(0, 5);
+		// $scope.phones = data;
+		$scope.phones = phone.query();
+	// });
   /*$scope.phones = [
     {"name": "Nexus S",
      "snippet": "Fast just got faster with Nexus S.",
@@ -18,5 +21,19 @@ function PhoneListCtrl($scope , $http) {
   ];*/
  
   $scope.orderProp = 'age';
-  $scope.hello = "Hello, World!"
+  
+}
+
+function PhoneDetailCtrl($scope, $routeParams, phone) {
+  // $scope.phoneId = $routeParams.phoneId;
+  // $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+    // $scope.phone = data;
+    // $scope.mainImageUrl = data.images[0];
+  // });
+  $scope.phone = phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+    $scope.mainImageUrl = phone.images[0];
+  });
+  $scope.setImage = function(imageUrl){
+  	$scope.mainImageUrl = imageUrl;
+  }
 }
--- app/js/services.js
+++ app/js/services.js
@@ -2,3 +2,9 @@
 
 /* Services */
 
+angular.module('phonecatServices', ['ngResource']).
+    factory('phone', function($resource){
+  return $resource('phones/:phoneId.json', {}, {
+    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+  });
+});
